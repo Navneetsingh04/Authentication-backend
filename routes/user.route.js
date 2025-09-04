9304889748476f7dfb8b29c49cbf7573a7b04252
Navneet Singh <navneetsingh1825@gmail.com>
2025-09-04 23:48:29 +0530

    refactor user routes to include additional endpoints and improve code organization

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,22 @@
-import express from 'express'
-import { register } from "../controllers/user.controller.js"
+import express from "express";
+import {
+  forgotPassword,
+  getuser,
+  login,
+  logout,
+  register,
+  resetPassword,
+  verifyOtp,
+} from "../controllers/user.controller.js";
+import { isAuthenticated } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.post("/register",register);
-
+router.post("/register", register);
+router.post("/otp-verification", verifyOtp);
+router.post("/login", login);
+router.post("/logout", isAuthenticated, logout);
+router.get("/me", isAuthenticated, getuser);
+router.post("/password/reset", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
 
 export default router;
